a0bbe597ee96d3541678d78cb58fb87544990e2a
Fred Nau <fnau001@gmail.com>
2019-06-19 16:56:03 -0400

    lab-numpy solutions completed 06/19

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,36 +1,44 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
+print(np.__version__)
+print(np.show_config())
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2, 3, 5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5, 2, 3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+isEqualArray = np.array_equal(a, b)
+print(isEqualArray)
 
 
 #8. Are you able to add a and b? Why or why not?
 
+if isEqualArray:
+    sumAB = a.__add__(b)
+    print(sumAB)
+else:
+    print("No, arrays a and b have unequal shapes, and therefore cannot be added")
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
